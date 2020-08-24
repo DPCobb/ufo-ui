dc205ae266e2a5159bb75774281d5c692e30bc9a
Dan Cobb <dancobb@gmail.com>
2020-08-23 22:55:08 -0400

    drawer start

---
+++ src/js/components/drawer.js
@@ -0,0 +1,50 @@
+export default class UFODrawer extends HTMLElement {
+    static get observedAttributes() {
+        ['title', 'state']
+    }
+    constructor() {
+        super();
+        this.attachShadow({
+            mode: 'open'
+        });
+        this.open = false;
+    }
+
+    toggleState() {
+        if (this.open == false) {
+            this.open = true
+            this.classList.toggle('open')
+            this.classList.toggle('close')
+        } else {
+            this.open == false
+            this.classList.toggle('open')
+            this.classList.toggle('close')
+        }
+    }
+
+    toggle() {
+        this.toggleState()
+    }
+
+    connectedCallback() {
+        const {
+            shadowRoot
+        } = this
+        const title = this.getAttribute('title')
+        shadowRoot.innerHTML = `
+            
+        `
+        this.classList.add('ufo-drawer')
+        this.classList.add('close')
+        this.setAttribute('tabindex', 0)
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name == 'state') {
+            this.toggleState.bind(this)
+        } else if (oldValue !== newValue) {
+            this.shadowRoot.innerHTML = `
+            `
+        }
+    }
+}
--- src/js/ufo-ui.js
+++ src/js/ufo-ui.js
@@ -2,8 +2,10 @@ import UFOAlert from './components/alert.js'
 import UFOButton from './components/button.js'
 import UFOPill from './components/pill.js'
 import UFOCard from './components/card.js'
+import UFODrawer from './components/drawer.js'
 
 customElements.define('ufo-alert', UFOAlert);
 customElements.define('ufo-button', UFOButton);
 customElements.define('ufo-pill', UFOPill);
 customElements.define('ufo-card', UFOCard);
+customElements.define('ufo-drawer', UFODrawer);
